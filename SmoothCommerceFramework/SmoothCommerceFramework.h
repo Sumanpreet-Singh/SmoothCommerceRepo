53443286a86d3d484f0053960406f149864862ea
MAC <mac@MACs-MacBook-Pro.local>
2018-07-01 00:57:03 -0400

    Initial Commit

---
+++ SmoothCommerceFramework/SmoothCommerceFramework.h
@@ -0,0 +1,19 @@
+//
+//  SmoothCommerceFramework.h
+//  SmoothCommerceFramework
+//
+//  Created by MAC on 01/07/18.
+//  Copyright © 2018 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SmoothCommerceFramework.
+FOUNDATION_EXPORT double SmoothCommerceFrameworkVersionNumber;
+
+//! Project version string for SmoothCommerceFramework.
+FOUNDATION_EXPORT const unsigned char SmoothCommerceFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmoothCommerceFramework/PublicHeader.h>
+
+
